2d6d4a616258326ad0f1d9c3117b68e3bac275b9
jacobpiirsalu <46936408+jacobpiirsalu@users.noreply.github.com>
2026-08-26 13:17:29 -0400

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def binsearch(L,R):
+            if L>R:
+                return -1
+            M = (L+R)//2
+            if nums[M] == target:
+                return M
+            elif nums[M]<target:
+                return binsearch(M+1,R)
+            elif nums[M]>target:
+                return binsearch(L,M-1)
+
+        return binsearch(0, len(nums)-1)
